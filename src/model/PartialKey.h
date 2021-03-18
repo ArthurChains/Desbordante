3b3f852893b24bf1fdcd09e1266268ccfe96a6c3
Maxim Strutovsky <strutovsky.m.a@gmail.com>
2021-03-18 11:51:41 +0300

    Strategy, Consumer refactoring

--- src/model/PartialKey.h
+++ src/model/PartialKey.h
@@ -7,12 +7,12 @@
 class PartialKey {
 public:
     double error_;
-    std::shared_ptr<Vertical> vertical_;
+    Vertical vertical_;
     double score_;
 
-    PartialKey(std::shared_ptr<Vertical> vertical, double error, double score) : error_(error), vertical_(std::move(vertical)), score_(score) {}
+    PartialKey(Vertical vertical, double error, double score)
+        : error_(error), vertical_(std::move(vertical)), score_(score) {}
 
-    std::string toString() const { return vertical_->toString() + "~>" + boost::lexical_cast<std::string>(error_) + boost::lexical_cast<std::string>(score_); }
-    //double getError() const { return error_; }
-    //int getArity() const { return lhs_->getColumns().size(); }
+    std::string toString() const { return vertical_.toString() + "~>"
+        + boost::lexical_cast<std::string>(error_) + boost::lexical_cast<std::string>(score_); }
 };
